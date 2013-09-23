27ff821658b7eb97aa68106b23ae10e466a8e894
Erik Jan de Wit <erikjan.dewit@gmail.com>
2013-09-23 08:57:37 +0200

    added support for iOS

--- src/ios/CDVAeroGearPlugin.m
+++ src/ios/CDVAeroGearPlugin.m
@@ -10,6 +10,40 @@
 
 @implementation CDVAeroGearPlugin
 
+- (void)generateOTP:(CDVInvokedUrlCommand*)command
+{
+    [self.commandDelegate runInBackground:^{
+        NSString *secret = [command.arguments objectAtIndex:0];
+
+        AGTotp *generator = [[AGTotp alloc] initWithSecret:[AGBase32 base32Decode:secret]];
+        NSString *totp = [generator generateOTP];
+
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:totp];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)readSecret:(CDVInvokedUrlCommand*)command
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *secret = [defaults objectForKey:@"secret"];
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:secret];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)storeSecret:(CDVInvokedUrlCommand*)command
+{
+    NSString *secret = [command.arguments objectAtIndex:0];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:secret forKey:@"secret"];
+    [defaults synchronize];
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
 - (void)generate:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
