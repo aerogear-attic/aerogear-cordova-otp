db1191dc55ab70d9e462e32b45369015b9d2cdd4
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-01-24 10:36:23 +0100

    updated jsdoc

--- www/aerogear-opt.js
+++ www/aerogear-opt.js
@@ -1,5 +1,28 @@
+/* AeroGear Cordova Plugin
+* https://github.com/aerogear/aerogear-pushplugin-cordova
+* JBoss, Home of Professional Open Source
+* Copyright Red Hat, Inc., and individual contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 (function (window, undefined) {
     this.AeroGear = {};
+
+    /**
+     * Construct Totp
+     * @status Stable
+     * @param {String} [secret] - The secret to use for generating the one time password
+     * @constructs AeroGear.Totp
+    */    
     AeroGear.Totp = function (secret) {
         this.secret = secret;
         return this;
